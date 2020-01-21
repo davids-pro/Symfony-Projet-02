9e26fe8cb1df3044443f71fab29d037f03a02364
davids-pro <54986025+davids-pro@users.noreply.github.com>
2020-01-21 13:44:23 +0100

    backup

--- src/Controller/GlobalController.php
+++ src/Controller/GlobalController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Post;
 use App\Repository\PostCategoryRepository;
 use App\Repository\PostRepository;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GlobalController extends AbstractController
@@ -31,7 +37,7 @@ class GlobalController extends AbstractController
         ]);
     }
     /**
-     * @Route("/articles/", name="articles")
+     * @Route("/articles", name="articles")
      */
     public function article(PostRepository $postRepository)
     {
@@ -40,6 +46,28 @@ class GlobalController extends AbstractController
         return $this->render('global/articles.html.twig', [
             'controller_name' => 'GlobalController',
             'articles' => $articles
+        ]);
+    }
+    /**
+     * @Route("/ajout", name="create-article")
+     */
+    public function create(Request $request, EntityManagerInterface $manager)
+    {
+        $article = new Post();
+        $form = $this->createFormBuilder($article)
+            ->add('title', TextType::class)
+            ->add('content', TextareaType::class)
+            ->getForm();
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $article->setDateCreated(new DateTime());
+            $article->setEnable(true);
+            $manager->persist($article);
+            $manager->flush();
+            return $this->redirectToRoute('articles');
+        }
+        return $this->render('global/ajout.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
